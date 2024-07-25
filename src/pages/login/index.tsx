5e77c16cf3fc559057b0db42e6d20739e2e1debf
JosielLinhar <iamlinhar@gmail.com>
2024-07-24 22:57:24 -0300

    iniciando projeto webcarros

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,17 @@
+import logoImg from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { Container } from "../../components/container";
+
+export function Login() {
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="Logo do site" className="w-full" />
+        </Link>
+
+        <form></form>
+      </div>
+    </Container>
+  );
+}
