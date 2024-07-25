150d590ee78214fa4ceafe9d7994300ee7b62deb
JosielLinhar <iamlinhar@gmail.com>
2024-07-24 23:33:41 -0300

    finalizando area de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,35 @@ import logoImg from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { Container } from "../../components/container";
 
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .nonempty("O campo email é obrigatório"),
+  password: z.string().nonempty("O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
@@ -10,7 +38,39 @@ export function Login() {
           <img src={logoImg} alt="Logo do site" className="w-full" />
         </Link>
 
-        <form></form>
+        <form
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+          >
+            Acessar
+          </button>
+        </form>
+
+        <Link to="/register">Ainda não possui uma conta? Cadastre-se</Link>
       </div>
     </Container>
   );
